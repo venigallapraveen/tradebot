b1eecec73120981c3eef593e5f1e9b3752a7ff6b
Praveen Venigalla <praveenvenigalla@Praveens-MacBook-Air.local>
2019-03-05 20:38:49 -0500

    built for heroku v2

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function (req, res) {
     res.sendFile(__dirname + "/dist/index.html");
 });
-const port = process.env.PORT || 80;
 app.listen(port);
 
 console.log("Server started...");
